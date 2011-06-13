93395efa801e757042127d054b391d9ed411451a
Bastian Widmer <b.widmer@dasrecht.net>
2011-06-14 00:00:27 +0200

    made 404 page better

--- 404.php
+++ 404.php
@@ -1,6 +1,15 @@
 <?php get_header(); ?>
 
+		<article class="post" id="post-<?php the_ID(); ?>">
+		  
+		<div class="eintrag">
+
+
 	<h2>Error 404 - Page Not Found</h2>
+	<p>This is somewhat embarrassing, isn&rsquo;t it?</p>
+		</div> <!-- eintrag -->
+
+		</article>
 
 <?php get_sidebar(); ?>
 
