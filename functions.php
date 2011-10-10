aefabf4352fc7c2ee229fadf2a51548fd701661b
Bastian Widmer <bastian.widmer@liip.ch>
2011-10-10 17:22:54 +0200

    fixed problem in functions.php1

--- functions.php
+++ functions.php
@@ -1,19 +1,7 @@
 <?php
 
 	// Add RSS links to <head> section
-	automatic_feed_links();
-
-	// Load jQuery
-	if ( !function_exists(core_mods) ) {
-		function core_mods() {
-			if ( !is_admin() ) {
-				wp_deregister_script('jquery');
-				wp_register_script('jquery', ("http://ajax.googleapis.com/ajax/libs/jquery/1.4.1/jquery.min.js"), false);
-				wp_enqueue_script('jquery');
-			}
-		}
-		core_mods();
-	}
+	add_theme_support( 'automatic-feed-links' );
 
 	// Clean up the <head>
 	function removeHeadLinks() {
